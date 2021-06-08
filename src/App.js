a282dbe11fdde27ec90b1aed2efe55c823cc0cb1
nonbunrah <rj.bamrah@gmail.com>
2021-06-08 15:42:40 -0700

    useFetch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import { useFetch } from "./useFetch";
 import {useForm} from "./useForm";
 
 const App = () => {
-  const [values, handleChange] = useForm({email: "", password: ""});
+  const [values, handleChange] = useForm({
+    email: "", 
+    password: "", 
+    firstName: ""
+  });
+  const [count, setCount] = useState(0);
+  const {data, loading} = useFetch(`http://numbersapi.com/${count}/trivia`)
+  // http://numbersapi.com/43/trivia
 
   return (
     <div>
+      <div>{!data ? "loading..." : data}</div>
+      <div>count: {count}</div>
+      <button onClick={() => setCount(c => c + 1)}>increment</button>
+      <br />
       <input 
         name="email" 
         value={values.email}
         placeholder="email"
+        onChange={handleChange}
+      />
+      <br />
+      <input 
+        name="firstName"
+        placeholder="first name"
+        value={values.firstName}
         onChange={handleChange}
       />
       <br />
